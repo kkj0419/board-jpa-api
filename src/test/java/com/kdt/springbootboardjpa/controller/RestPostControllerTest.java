955383ee5c58e26c1093dafcd5f868746668fe29
kkj0419 <kkhi0419@gmail.com>
2022-06-13 21:58:11 +0900

    refactor : 불필요한 import 삭제

--- src/test/java/com/kdt/springbootboardjpa/controller/RestPostControllerTest.java
+++ src/test/java/com/kdt/springbootboardjpa/controller/RestPostControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
